efd82975a44985b87b1193fe6f90b00186bd607e
d-static <92041779+d-static@users.noreply.github.com>
2021-12-09 19:32:23 -0500

    Delete fibonacci.d

--- fibonacci.d
+++
@@ -1,4 +0,0 @@
-long fibonacciRecursive(uint n) {
-    // Recursive solution with ternary operator. Just to show some syntax of D.
-    return (n < 2) ? n : fibonacciRecursive(n-1) + fibbonacciRecursive(n-2);
-}
